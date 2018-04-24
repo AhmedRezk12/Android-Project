12f16821ec3f51334407a773778beafde1b0157e
AhmedRezk12 <38145787+AhmedRezk12@users.noreply.github.com>
2018-04-25 00:30:16 +0200

    done
    add

---
+++ Android/app/src/main/java/com/example/eslam/project/ahmed.java
@@ -0,0 +1,13 @@
+package com.example.eslam.project;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ahmed extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ahmed);
+    }
+}
