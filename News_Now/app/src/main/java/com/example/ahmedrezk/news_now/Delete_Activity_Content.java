0b3ed22495a72fcff0c6faa9eaef36793badb0c1
EslamOsamaAhmed <37809837+EslamOsamaAhmed@users.noreply.github.com>
2018-04-25 16:32:14 +0200

    Project
    Project

---
+++ News_Now/app/src/main/java/com/example/ahmedrezk/news_now/Delete_Activity_Content.java
@@ -0,0 +1,13 @@
+package com.example.ahmedrezk.news_now;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Delete_Activity_Content extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_delete___content);
+    }
+}
---
+++ News_Now/app/src/main/java/com/example/ahmedrezk/news_now/Update_Activity_Content.java
@@ -0,0 +1,13 @@
+package com.example.ahmedrezk.news_now;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Update_Activity_Content extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_update___content);
+    }
+}
